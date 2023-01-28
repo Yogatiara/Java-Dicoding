49e96aa2ae5b1fe76fddc96807bb2bf42bb2aa67
yoga <11211086@student.itk.ac.id>
2023-01-28 23:43:49 +0800

    Menambah function(InputFunction, MethodOverloading) dan accesmodifier(private dan protected)

---
+++ StrukturDasar/src/com/dicoding/javafundamental/OOP/propertimetode/Hewan.java
@@ -0,0 +1,17 @@
+package com.dicoding.javafundamental.OOP.propertimetode;
+
+public class Hewan {
+    // Properti atau fields
+
+    // Inisiasi dengan nilai inisial
+    double tinggi = 30;
+    double berat = 3;
+
+    // Inisiasi melalui konstruktor
+    int umur;
+
+    // Konstruktor dengan parameter
+    Hewan(int umur) {
+        this.umur = umur;
+    }
+}

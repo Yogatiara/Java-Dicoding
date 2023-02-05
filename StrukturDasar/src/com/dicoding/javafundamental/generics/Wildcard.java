42fc1b86a4977b914e8bedb3366b1c1461e703ae
yoga <11211086@student.itk.ac.id>
2023-02-05 09:59:04 +0800

    Menambahkan modul generics

---
+++ StrukturDasar/src/com/dicoding/javafundamental/generics/Wildcard.java
@@ -0,0 +1,42 @@
+package com.dicoding.javafundamental.generics;
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+public class Wildcard {
+    public static void main(String[] args) {
+        List<String> ls = new ArrayList();
+        ls.add("String1");
+        ls.add("String2");
+
+        print(ls); // Apakah baris ini valid?
+
+        Collection<Planet> cp = new ArrayList();
+        cp.add(new Planet("Mercury", 0.06));
+        cp.add(new Planet("Venus", 0.82));
+
+        print(cp); // Apakah baris ini valid?
+    }
+
+    public static void print(Collection<?> collection) {
+        for (Object o : collection) {
+            System.out.println(o);
+        }
+    }
+
+//    static void arrayToCollection(Object[] a, Collection<?> c) {
+//        for (Object o : a) {
+//            c.add(o); // baris ini tidak valid
+//        }
+//
+//    }
+
+    static <T> void arrayToCollection(T[] a, Collection<T> c) {
+        for (T o : a) {
+            c.add(o); // baris ini valid
+        }
+    }
+
+
+}
